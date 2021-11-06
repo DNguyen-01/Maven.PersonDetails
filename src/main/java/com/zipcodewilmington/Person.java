8df44c3f4f476351b1272edd08c234dfe9b97773
David Nguyen <davebox309@gmail.com>
2021-11-05 23:26:46 -0400

    completedLab

--- src/main/java/com/zipcodewilmington/Person.java
+++ src/main/java/com/zipcodewilmington/Person.java
@@ -7,21 +7,21 @@ public class Person {
     private final String firstName;
     private final String lastName;
 
-    public Person(String firstName, String lastName) {
+    public Person(String firstName, String lastName) { //provides the attributes to the blueprint - these are constructors
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public String getFirstName() {
+    public String getFirstName() { //getters
         return firstName;
     }
 
-    public String getLastName() {
+    public String getLastName() { //getters
         return lastName;
     }
 
-    @Override
-    public String toString() {
+    @Override // all objects have inate methods, .toStrings-gives us the memory address (object type @ series of number)
+    public String toString() { //created method for us to dictate what we want the toString to do.
         return new StringBuilder()
                 .append("\nMy first name is " + firstName)
                 .append("\nMy last name is " + lastName)
--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -7,20 +7,21 @@ public class PersonHandler {
     private final Person[] personArray;
 
     public PersonHandler(Person[] personArray) {
-        this.personArray = personArray;
-    }
+        this.personArray = personArray;}
 
-    public String whileLoop() {
-        String result = "";
+    public String whileLoop(){ //while loops requires you to have something to end the loop
+        String result ="";
+        int counter = 0;
         // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
+        while(counter < this.personArray.length) { // begin loop - while the counter(position 0) is less than the constructor (this.personArray.length) continue the counter
+            // while `counter` is less than length of array
+            Person currentPerson = personArray[counter];  // use `counter` to identify the `current Person` in the array - this variable holds the current position of the while loop
+            String currentPersonString = currentPerson.toString(); // get `string Representation` of `currentPerson`
+            result += currentPersonString;  // append `stringRepresentation` to `result` variable
+            System.out.println("Current Person(counter="+ counter + ") : " + currentPersonString);
+            counter++;  // end loop, by incrementing the counter until it meets the condition
+        }
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
         return result;
     }
 
@@ -28,32 +29,33 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for(int i=0; i<personArray.length; i++) { // identify initial value,  // identify terminal condition,// identify increment
+            // use the above clauses to declare for-loop signature
+            Person currentPerson = personArray[i];   // use `counter` to identify the `current Person` in the array
+            String currentPersonString = currentPerson.toString();   // get `string Representation` of `currentPerson
+            result += currentPersonString; // append `stringRepresentation` to `result` variable
+            // end loop - because it ends after the condition are met
+            System.out.println("Current Person(i="+ i + ") : " + currentPersonString);
+        }
 
         return result;
+
     }
 
 
 
-    public String forEachLoop() {
+    public String forEachLoop() { //will iterate on each element in a loop
         String result = "";
+
         // identify array's type
         // identify array's variable-name
 
         // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
+        for (Person currentPerson : personArray) { // (for each current person in person array do something) - verbalize form - (variable encapsulating the : array)
+            String currentPersonString = currentPerson.toString(); // get `string Representation` of `currentPerson`
+            result += currentPersonString;
+            System.out.println("Current Person : " + currentPersonString);
+        }
 
         return result;
     }
